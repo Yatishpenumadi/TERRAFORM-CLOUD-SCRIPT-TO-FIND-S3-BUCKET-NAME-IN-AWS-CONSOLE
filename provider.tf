6c0494457c0b11ff6fa45c8691016ad0cfcf5b02
Yatishpenumadi <110468458+Yatishpenumadi@users.noreply.github.com>
2022-09-24 15:32:49 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,1 +1,27 @@
 
+terraform {
+
+required_version = "~> 1.3.0"
+
+required_providers {
+
+aws = {
+
+source = "hashicorp/aws"
+
+version = "~> 4.24.0"
+
+}
+
+}
+
+}
+
+
+
+provider "aws" {
+
+region = var.aws_region
+
+}
+
